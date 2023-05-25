6bf5870fb7781ad4b4699b869e12d5d3445baaa5
Alanood <111198226+anoudab@users.noreply.github.com>
2023-05-25 12:35:04 +0300

    Create Bus.cs

---
+++ Bus Management System/Entities/Bus.cs
@@ -0,0 +1,17 @@
+namespace BusManagementSystem.Entities
+{ 
+
+    public class Bus
+    {
+        public int BusID
+        { get; set; }
+
+        public int NoOfSeats
+        { get; set; }
+
+        public string Region
+        { get; set; }   
+
+    }
+
+}
