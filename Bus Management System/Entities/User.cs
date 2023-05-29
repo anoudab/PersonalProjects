bd96b374a4c9da70b1560afb2458fbe19d3a6e0f
Alanood <111198226+anoudab@users.noreply.github.com>
2023-05-29 19:47:58 +0300

    Create User.cs

---
+++ Bus Management System/Entities/User.cs
@@ -0,0 +1,9 @@
+namespace BMS.Entities
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
