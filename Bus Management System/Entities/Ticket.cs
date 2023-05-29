b6036eeda29fd7a5481f151e61327535b88b2b96
Alanood <111198226+anoudab@users.noreply.github.com>
2023-05-29 19:46:57 +0300

    Update Ticket.cs

--- Bus Management System/Entities/Ticket.cs
+++ Bus Management System/Entities/Ticket.cs
@@ -1,25 +1,14 @@
-namespace BusManagementSystem.Entities
+namespace BMS.Entities
 {
     public class Ticket
     {
-            public int TicketID
-            { get; set; }
-
-            public string PassengerName
-            { get; set; }
-
-            public double Price
-            { get; set; }
-
-            public string StartDestination
-            { get; set; }
-
-            public string ArrivalDestination
-            { get; set; }
-
-            public int BusID
-            { get; set; }
-  
+            public int TicketId { get; set; }
+            public string Available { get; set; }
+            public double Price { get; set; }
+            public string StartDestination { get; set; }
+            public string ArrivalDestination { get; set; }
+            public int BusId { get; set; }
+            public int UserId { get; set; }
 
     }
 }
