e24a6b75330f27b101cf1b2ad67bc42c07475dea
Alanood <111198226+anoudab@users.noreply.github.com>
2023-05-29 19:45:13 +0300

    Update QueryConstants.cs

--- Bus Management System/Constants/QueryConstants.cs
+++ Bus Management System/Constants/QueryConstants.cs
@@ -1,9 +1,31 @@
-namespace BusManagementSystem.Constants
+namespace BMS.Constants
 {
     public class QueryConstants
     {
-        public const string SelectAll = "SELECT * FROM Ticket";
-        public const string SelectAllWhere = "SELECT * FROM Ticket where TicketID = ";
+        //Select
+        //Admin
+        public const string SelectAllTicket = "SELECT * FROM Ticket";//alanood
+        public const string SelectAllBuses = "SELECT * FROM Bus";//alanood+Rafeef 
+        //Client
+        public const string SelectAllAvailableTicket = "SELECT * FROM Ticket where Available =@Available ";//Rafeef
+        public const string SelectAllClintTicket= "SELECT * FROM Ticket where UserId =@UserId ";//Rafeef
+        public const string SelectwhereTicket = "SELECT * FROM Ticket where TicketId =@TicketId ";//Rafeef
 
+        //Insert 
+        public const string CreateTicket = "INSERT INTO Ticket (Available,Price,StartDestination,ArrivalDestination,BusId)" +
+            "VALUES (@Available,@Price,@StartDestination,@ArrivalDestination,@BusId)";//alanood+Rafeef
+
+        //Update
+        //Admin
+        public const string UpdateTicket = "UPDATE Ticket SET Available = @Available, Price = @Price, StartDestination " +
+            "= @StartDestination, ArrivalDestination = @ArrivalDestination, BusId = @BusId ,UserId = @UserId WHERE TicketID = @TicketId";//alanood+Rafeef
+
+        public const string UpdateBus = "UPDATE Bus SET NoOfSeats = @NoOfSeats, Region = @Region, DriverName = @DriverName, Plate = @Plate  WHERE BusId = @BusId";// alanood+Rafeef
+        //Client
+        public const string BookTicket = "UPDATE Ticket SET Available = 'false' ,UserId = @UserId WHERE TicketId =@TicketId";//Rafeef
+        public const string CancelBookedTicket = "UPDATE Ticket SET Available = 'true' ,UserId = NULL WHERE TicketId =@TicketId ";//Rafeef
+
+        //Delete
+        public const string DeleteTicket = "DELETE FROM Ticket WHERE TicketId=@TicketId";//Rafeef
     }
 }
