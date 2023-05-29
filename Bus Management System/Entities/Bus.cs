7b49537c8525ebc965ef8f3b449998100402de11
Alanood <111198226+anoudab@users.noreply.github.com>
2023-05-29 19:46:19 +0300

    Update Bus.cs

--- Bus Management System/Entities/Bus.cs
+++ Bus Management System/Entities/Bus.cs
@@ -1,17 +1,14 @@
-namespace BusManagementSystem.Entities
+namespace BMS.Entities
 { 
 
     public class Bus
     {
-        public int BusID
-        { get; set; }
-
-        public int NoOfSeats
-        { get; set; }
-
-        public string Region
-        { get; set; }   
-
+        public int BusId { get; set; }
+        public int NoOfSeats { get; set; }
+        public string Region { get; set; }
+        public string DriverName { get; set; }
+        public string Palte { get; set; }
+        public object Plate { get; internal set; }
     }
 
 }
